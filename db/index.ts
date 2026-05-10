7c4bccdcf3b6819af49747ba80265300094f70b9
souravslg <sourav.slg1993@gmail.com>
2026-05-11 01:54:49 +0530

    fix: db/index.ts D1Database typescript error

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,9 @@
-﻿import { getRequestContext } from '@cloudflare/next-on-pages';
+import { getRequestContext } from '@cloudflare/next-on-pages';
 
 export function getDb() {
   const env = getRequestContext().env as any;
   if (!env || !env.DB) {
     throw new Error('D1 database binding (DB) is not available');
   }
-  return env.DB as D1Database;
+  return env.DB as any;
 }
